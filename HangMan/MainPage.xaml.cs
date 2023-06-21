54a171cdb91dd0a3199085d4eda5116d03e2866e
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2023-06-21 21:07:32 +0300

    Selecting words done

--- HangMan/MainPage.xaml.cs
+++ HangMan/MainPage.xaml.cs
@@ -1,12 +1,53 @@
-﻿namespace HangMan;
+﻿using System.Diagnostics;
+using HangMan.Models;
+
+namespace HangMan;
 
 public partial class MainPage : ContentPage
 {
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    List<string> Words = new List<string>()
+    {
+        "python",
+        "javascript",
+        "maui",
+        "csharp",
+        "mongodb",
+        "sql",
+        "xaml",
+        "word",
+        "excel",
+        "powerpoint",
+        "code",
+        "hotreload",
+        "snippets"
+    };
+
+    List<char> guess = new List<char>();
+
+    public string Answer { get; set; } = string.Empty;
+
+    Materials materials = new Materials();
+
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = materials;
+        PickWord();
+        CalculateWord(Answer,guess);
+    }
+
+    private void PickWord()
+    {
+        Answer = Words[new Random().Next(0, Words.Count)];
+        Debug.WriteLine(Answer);
+    }
+
+    private void CalculateWord(string answer,List<char> guessed)
+    {
+        var temp = answer.Select(x => (guessed.IndexOf(x) >= 0 ? x : '_'));
+        materials.Spotlight = string.Join(' ',temp);
+    }
 
 }
 
---
+++ HangMan/Models/Materials.cs
@@ -0,0 +1,68 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HangMan.Models;
+
+public class Materials : INotifyPropertyChanged
+{
+    private string spotlight;
+    private List<char> letters;
+    private string message;
+    private string gameStatus;
+    private string currentImage;
+
+    public string Spotlight
+    {
+        get => spotlight;
+        set
+        {
+            spotlight = value;
+            OnPropertyChanged();
+        }
+    }
+    public List<char> Letters
+    {
+        get => letters;
+        set
+        {
+            letters = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Message
+    {
+        get => message;
+        set
+        {
+            message = value;
+            OnPropertyChanged();
+        }
+    }
+    public string GameStatus
+    {
+        get => gameStatus;
+        set
+        {
+            gameStatus = value;
+            OnPropertyChanged();
+        }
+    }
+    public string CurrentImage
+    {
+        get => currentImage;
+        set
+        {
+            currentImage = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
